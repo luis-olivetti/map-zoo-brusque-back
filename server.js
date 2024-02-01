36e023c419f0528317845b2bd79c69bef7c054e1
Luis Olivetti <luuisf92@gmail.com>
2024-02-01 09:52:20 -0300

    chore: added authorization

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const cors = require('cors');
 const jsonServer = require('json-server');
+const basicAuth = require('basic-auth');
+require('dotenv').config();
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// Habilita o CORS para todos os domínios
 server.use(cors());
-
-// Adiciona middleware padrão (logger, manipulação de erros, etc.)
 server.use(middlewares);
 
-// Usa o roteador JSON-Server com o middleware CORS
+server.use((req, res, next) => {
+  const user = basicAuth(req);
+
+  const username = process.env.JSON_SERVER_USERNAME;
+  const password = process.env.JSON_SERVER_PASSWORD;
+
+  if (!user || user.name !== username || user.pass !== password) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.status(401).send('Unauthorized');
+  }
+
+  next();
+});
+
 server.use(router);
 
-// Define a porta para ouvir
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`JSON Server está rodando em http://localhost:${PORT}`);
