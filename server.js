d391e69c87472769ebdd43d0f1c1bb77d56d742e
Luis Olivetti <luuisf92@gmail.com>
2024-02-11 14:40:50 -0300

    Add gcp.json and *.http to .gitignore, and update dependencies in package.json

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const cors = require('cors');
 const jsonServer = require('json-server');
-const basicAuth = require('basic-auth');
+const jwt = require('jsonwebtoken');
 const { Storage } = require('@google-cloud/storage');
+const crypto = require('crypto');
 require('dotenv').config();
 
 const server = jsonServer.create();
@@ -17,170 +18,245 @@ const bucket = storage.bucket(bucketName);
 
 server.use(cors());
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
+const jwtSecret = process.env.JWT_SECRET;
+
+// Middleware para verificar o token JWT
 server.use((req, res, next) => {
-  const user = basicAuth(req);
-
-  const username = process.env.JSON_SERVER_USERNAME;
-  const password = process.env.JSON_SERVER_PASSWORD;
-  const origin = req.headers.origin;
-  const trustedOrigins = [process.env.ORIGIN_HTTPS, process.env.ORIGIN_HTTP];
-
-  if (!trustedOrigins.includes(origin)) {
-    return res.status(403).send('Forbidden - Invalid origin');
+  if (req.path === '/login') {
+    return next();
   }
 
-  if (!user || user.name !== username || user.pass !== password) {
-    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-    return res.status(401).send('Unauthorized');
+  if (req.method === 'GET' && req.path.startsWith('/markers')) {
+    return next();
   }
 
-  next();
+  const token = req.headers.authorization;
+
+  if (token && token.startsWith('Bearer ')) {
+    const tokenWithoutBearer = token.slice(7);
+    try {
+      const decoded = jwt.verify(tokenWithoutBearer, jwtSecret);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      return res.status(403).send('Invalid token');
+    }
+  } else {
+    res.status(401).send('Token not provided');
+  }
 });
 
+function hashString(value) {
+  return crypto.createHash('sha256').update(value).digest('hex');
+}
 
-// Middleware para manipular a rota "markers"
-server.use('/markers', async (req, res, next) => {
+server.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const hashedUsername = hashString(username);
+  const hashedPassword = hashString(password);
+
+  if (hashedUsername === process.env.JSON_SERVER_USERNAME && hashedPassword === process.env.JSON_SERVER_PASSWORD) {
+    const token = jwt.sign({ username }, jwtSecret, { expiresIn: '10s' });
+    res.json({ token });
+  } else {
+    res.status(401).send('Invalid credentials');
+  }
+});
+
+server.post('/markers', async (req, res) => {
   const file = bucket.file('db.json');
 
-  if (req.method === 'GET') {
-    if (/\/\d+$/.test(req.url)) {
-      const id = req.url.split('/').pop(); // Obtém o ID do final da URL
+  const secondaryToken = req.headers.secondarytoken;
 
-      if (!id || isNaN(id)) {
-        console.error('ID inválido fornecido.');
-        res.status(400).send('Bad Request');
-        return;
-      }
+  if (!secondaryToken) {
+    res.status(400).send('Bad Request');
+    return;
+  }
 
-      try {
-        const [content] = await file.download();
-        const data = JSON.parse(content.toString());
+  const hashedSecondaryToken = hashString(secondaryToken);
 
-        if ('trucksOnMap' in data) {
-          const truck = data.trucksOnMap.find(item => item.id === parseInt(id));
+  if (hashedSecondaryToken !== process.env.SECONDARY_TOKEN) {
+    res.status(403).send('Forbidden');
+    return;
+  }
 
-          if (truck) {
-            res.json(truck);
-          } else {
-            console.error('Item não encontrado para o ID fornecido.');
-            res.status(404).send('Not Found');
-          }
-        } else {
-          console.error('A propriedade "trucksOnMap" não foi encontrada no arquivo JSON.');
-          res.status(500).send('Internal Server Error');
-          return;
-        }
-      } catch (error) {
-        console.error('Erro ao ler o arquivo do Google Cloud Storage:', error);
-        res.status(500).send('Internal Server Error');
-      }
-    } else {
-      // Para GET, leia o arquivo do bucket
-      try {
-        const [content] = await file.download();
-        const data = JSON.parse(content.toString());
+  try {
+    const [content] = await file.download();
+    const data = JSON.parse(content.toString());
+    const newData = req.body;
 
-        // Verifique se a propriedade 'trucksOnMap' existe no objeto
-        if ('trucksOnMap' in data) {
-          res.json(data.trucksOnMap);
-        } else {
-          console.error('A propriedade "trucksOnMap" não foi encontrada no arquivo JSON.');
-          res.status(500).send('Internal Server Error');
-          return;
-        }
-      } catch (error) {
-        console.error('Erro ao ler o arquivo do Google Cloud Storage:', error);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
+    if (!Array.isArray(data.trucksOnMap)) {
+      data.trucksOnMap = [];
     }
 
-  } else if (req.method === 'POST' || req.method === 'PUT') {
-    let rawData = '';
+    newData.id = data.trucksOnMap.length + 1;
+    data.trucksOnMap.push(newData);
 
-    req.on('data', (chunk) => {
-      rawData += chunk;
-    });
+    await file.save(JSON.stringify(data));
 
-    req.on('end', async () => {
-      // Processar dados recebidos (para POST ou PUT)
-      try {
-        const [content] = await file.download();
-        const data = JSON.parse(content.toString());
+    res.json(newData);
+  } catch (error) {
+    console.error('Erro ao processar dados ou salvar no Google Cloud Storage:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-        // Adicionar ou atualizar dados com base nos dados recebidos
-        // (rawData contém os dados do corpo da solicitação)
-        const newData = JSON.parse(rawData);
+server.put('/markers/:id', async (req, res) => {
+  const file = bucket.file('db.json');
 
-        if (!Array.isArray(data.trucksOnMap)) {
-          data.trucksOnMap = [];
-        }
+  try {
+    const [content] = await file.download();
+    const data = JSON.parse(content.toString());
+    const newData = req.body;
 
-        if (req.method === 'POST') {
-          // Adicionar novo item
-          newData.id = data.trucksOnMap.length + 1;
-          data.trucksOnMap.push(newData);
-        } else if (req.method === 'PUT') {
-          // Atualizar item existente
-          const index = data.trucksOnMap.findIndex(item => item.id === newData.id);
+    // Encontrar o índice do item com o ID fornecido
+    const index = data.trucksOnMap.findIndex(item => item.id === parseInt(req.params.id));
 
-          if (index !== -1) {
-            data.trucksOnMap[index] = newData;
-          } else {
-            console.error('Item não encontrado para atualização.');
-            res.status(404).send('Not Found');
-            return;
-          }
-        }
+    if (index !== -1) {
+      // Atualizar o item existente
+      data.trucksOnMap[index] = newData;
+
+      // Salvar os dados modificados de volta no arquivo do bucket
+      await file.save(JSON.stringify(data));
+
+      // Responder com os dados modificados
+      res.json(newData);
+    } else {
+      console.error('Item não encontrado para atualização.');
+      res.status(404).send('Not Found');
+    }
+  } catch (error) {
+    console.error('Erro ao processar dados ou salvar no Google Cloud Storage:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Handler para GET em /markers
+server.get('/markers', async (req, res) => {
+  const file = bucket.file('db.json');
+
+  try {
+    const [content] = await file.download();
+    const data = JSON.parse(content.toString());
+
+    if ('trucksOnMap' in data) {
+      res.json(data.trucksOnMap);
+    } else {
+      console.error('A propriedade "trucksOnMap" não foi encontrada no arquivo JSON.');
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+  } catch (error) {
+    console.error('Erro ao ler o arquivo do Google Cloud Storage:', error);
+    res.status(500).send('Internal Server Error');
+    return;
+  }
+});
+
+// Handler para GET em /markers/:id
+server.get('/markers/:id', async (req, res) => {
+  const file = bucket.file('db.json');
+
+  const secondaryToken = req.headers.secondarytoken;
+
+  if (!secondaryToken) {
+    res.status(400).send('Bad Request');
+    return;
+  }
+
+  const hashedSecondaryToken = hashString(secondaryToken);
+
+  if (hashedSecondaryToken !== process.env.SECONDARY_TOKEN) {
+    res.status(403).send('Forbidden');
+    return;
+  }
+
+  const id = req.params.id;
+
+  if (!id || isNaN(id)) {
+    console.error('ID inválido fornecido.');
+    res.status(400).send('Bad Request');
+    return;
+  }
+
+  try {
+    const [content] = await file.download();
+    const data = JSON.parse(content.toString());
+
+    if ('trucksOnMap' in data) {
+      const truck = data.trucksOnMap.find(item => item.id === parseInt(id));
+
+      if (truck) {
+        res.json(truck);
+      } else {
+        console.error('Item não encontrado para o ID fornecido.');
+        res.status(404).send('Not Found');
+      }
+    } else {
+      console.error('A propriedade "trucksOnMap" não foi encontrada no arquivo JSON.');
+      res.status(500).send('Internal Server Error');
+    }
+  } catch (error) {
+    console.error('Erro ao ler o arquivo do Google Cloud Storage:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Handler para DELETE em /markers/:id
+server.delete('/markers/:id', async (req, res) => {
+  const file = bucket.file('db.json');
+
+  const secondaryToken = req.headers.secondarytoken;
+
+  if (!secondaryToken) {
+    res.status(400).send('Bad Request');
+    return;
+  }
+
+  const hashedSecondaryToken = hashString(secondaryToken);
+
+  if (hashedSecondaryToken !== process.env.SECONDARY_TOKEN) {
+    res.status(403).send('Forbidden');
+    return;
+  }
+
+  const id = req.params.id;
+
+  if (!id || isNaN(id)) {
+    console.error('ID inválido fornecido.');
+    res.status(400).send('Bad Request');
+    return;
+  }
+
+  try {
+    const [content] = await file.download();
+    const data = JSON.parse(content.toString());
+
+    if ('trucksOnMap' in data) {
+      const index = data.trucksOnMap.findIndex(item => item.id === parseInt(id));
+
+      if (index !== -1) {
+        data.trucksOnMap.splice(index, 1);
 
         // Salvar os dados modificados de volta no arquivo do bucket
         await file.save(JSON.stringify(data));
 
         // Responder com os dados modificados (opcional)
-        res.json(newData);
-      } catch (error) {
-        console.error('Erro ao processar dados ou salvar no Google Cloud Storage:', error);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-    });
-  } else if (req.method === 'DELETE' && /\/\d+$/.test(req.url)) {
-    const id = req.url.split('/').pop();
-
-    if (!id || isNaN(id)) {
-      console.error('ID inválido fornecido.');
-      res.status(400).send('Bad Request');
-      return;
-    }
-
-    try {
-      const [content] = await file.download();
-      const data = JSON.parse(content.toString());
-
-      if ('trucksOnMap' in data) {
-        const index = data.trucksOnMap.findIndex(item => item.id === parseInt(id));
-
-        if (index !== -1) {
-          data.trucksOnMap.splice(index, 1);
-
-          // Salvar os dados modificados de volta no arquivo do bucket
-          await file.save(JSON.stringify(data));
-
-          // Responder com os dados modificados (opcional)
-          res.json(data.trucksOnMap);
-        } else {
-          console.error('Item não encontrado para exclusão.');
-          res.status(404).send('Not Found');
-        }
+        res.json(data.trucksOnMap);
       } else {
-        console.error('A propriedade "trucksOnMap" não foi encontrada no arquivo JSON.');
-        res.status(500).send('Internal Server Error');
+        console.error('Item não encontrado para exclusão.');
+        res.status(404).send('Not Found');
       }
-    } catch (error) {
-      console.error('Erro ao ler ou salvar o arquivo do Google Cloud Storage:', error);
+    } else {
+      console.error('A propriedade "trucksOnMap" não foi encontrada no arquivo JSON.');
       res.status(500).send('Internal Server Error');
     }
+  } catch (error) {
+    console.error('Erro ao ler ou salvar o arquivo do Google Cloud Storage:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
